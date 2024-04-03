56ec6a11dc20da0fa2b8c0be375b83be21da9e61
PauloSm <pc_s89@hotmail.com>
2024-04-03 15:56:31 -0300

    feat: added main logic

--- cmd/go-downloader/main.go
+++ cmd/go-downloader/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"go-downloader/pkg/downloader"
+	"go-downloader/pkg/storage"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Missing params: go-downloader <URL> <File Path>")
+		return
+	}
+
+	url := os.Args[1]
+	savePath := os.Args[2]
+
+	httpDownloader := downloader.NewHttpDownloader()
+
+	localStorage := storage.NewLocalStorage()
+
+	downloadService := downloader.NewService(httpDownloader, localStorage)
+
+	err := downloadService.DownloadFile(url, savePath)
+	if err != nil {
+		fmt.Printf("Error when downloading the file: %v\n", err)
+		return
+	}
+
+	fmt.Println("The download has finished", savePath)
+}
+
